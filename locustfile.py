04f7790f3c7148be380b2946bc4cfced5769f41a
EL Abquina <eladio@pantheon.io>
2019-12-06 22:17:47 +0800

    DrupalCamp Manila 2020

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,3 @@
-## TODO - convert to drupal8.j2 template
 import os
 import string
 import random
@@ -6,8 +5,8 @@ import time
 import pytest
 import json
 import selenium
-from faker import Faker
 from bs4 import BeautifulSoup
+from faker import Faker
 from locust import HttpLocust, TaskSet, task, events, between
 from locust.clients import HttpSession
 from pyquery import PyQuery
@@ -18,13 +17,14 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
-## TODO - transfer Helper Functions in a separate module (helper.py)
 ### HELPER FUNCTIONS Section
+# To use, import helper 
+#
 ## Drupal 8 - Get form_build_id value via parsing
 def get_form_build_id(self, request_path):
     # Get form build ID to pass back to Drupal on login.
     response = self.client.get(request_path)
-    parsed_html = BeautifulSoup(response.content, 'lxml')
+    parsed_html = BeautifulSoup(response.content, "lxml")
     form_build_id = parsed_html.body.find('input', {'name': 'form_build_id'})['value']
     return form_build_id
 
@@ -32,7 +32,7 @@ def get_form_build_id(self, request_path):
 def get_form_token(self, request_path):
     # Get form build ID to pass back to Drupal on login.
     response = self.client.get(request_path)
-    parsed_html = BeautifulSoup(response.content, 'lxml')
+    parsed_html = BeautifulSoup(response.content, "lxml")
     form_token = parsed_html.body.find('input', {'name': 'form_token'})['value']
     return form_token
 
@@ -40,12 +40,12 @@ def get_form_token(self, request_path):
 def get_random_article(self, source_path, search_class, comment_form_id):
     resp = self.client.get(source_path)
 
-    parsed_html = BeautifulSoup(resp.content, 'lxml')
+    parsed_html = BeautifulSoup(resp.content, "lxml")
     article_links = [link['href'] for link in parsed_html.body.findAll("a", {"class": search_class})]
     random_article_path = random.choice(article_links)
 
     # Parse returned article for comment form ID and post path
-    random_article_html = BeautifulSoup(self.client.get(random_article_path).content, "lxml")
+    random_article_html = BeautifulSoup(self.client.get(random_article_path).content)
     comment_post_path = random_article_html.body.find('form', {'id': comment_form_id})['action']
     return { "random_article_path": random_article_path, "comment_post_path": comment_post_path }
 
@@ -64,13 +64,13 @@ class TestKedbloginsearch():
     # Test name: kedb-login-search
     # Step # | name | target | value | comment
     # 1 | open | / |  | 
-    self.driver.get("http://dev-ps-loadtest-dummy.pantheonsite.io/")
+    self.driver.get("https://dev-ps-loadtest-dummy.pantheonsite.io/")
     # 2 | click | id=edit-name |  | 
     self.driver.find_element(By.ID, "edit-name").click()
     # 3 | type | id=edit-name | eladio@getpantheon.com | 
-    self.driver.find_element(By.ID, "edit-name").send_keys("eladio@getpantheon.com")
+    self.driver.find_element(By.ID, "edit-name").send_keys("pantheon")
     # 4 | type | id=edit-pass | [Pantheon]1234 | 
-    self.driver.find_element(By.ID, "edit-pass").send_keys("[Pantheon]1234")
+    self.driver.find_element(By.ID, "edit-pass").send_keys("pantheon")
     # 5 | click | id=edit-submit |  | 
     self.driver.find_element(By.ID, "edit-submit").click()
     # 6 | click | id=facetapi-link--556 |  | 
@@ -82,7 +82,7 @@ class TestKedbloginsearch():
     # 9 | setWindowSize | 960x1008 |  | 
     self.driver.set_window_size(960, 1008)
 
-### Anon.T Anonymous TaskSet
+## Anon.T Anonymous TaskSet
 class AnonymousTaskSet(TaskSet):
 
     ## Anon.1.0 Random URLs from Homepage
@@ -100,7 +100,7 @@ class AnonymousTaskSet(TaskSet):
                 url = l.attrib["href"]
                 if url != "/user/logout":
                     # Ensure we only run links of the target site, excluding specific domains too
-                    if url != "//dev-ps-loadtest-dummy.pantheonsite.io" and url is not None and "#" not in url and (url.startswith('http://dev-ps-loadtest-dummy.pantheonsite.io') or url.startswith('/')):
+                    if url != "//dev-ps-loadtest-dummy.pantheonsite.io" and url is not None and "#" not in url and (url.startswith('https://dev-ps-loadtest-dummy.pantheonsite.io') or url.startswith('/')):
                         self.urls_on_current_page.append(l.attrib["href"])\
                         
     @task(1) 
@@ -213,17 +213,12 @@ class AnonymousTaskSet(TaskSet):
 
     ## Anon.7.0 API Endpoints (REST/JSON:API)
     ## TODO - actual endpoints functions and helpers
-    def retrieve_all_endpoints(self):
-        self.client.get("/json/api/endpoints/list")
-
-    @task(1)
-    def retrieve_endpoint(self):
-        self.client.get("/json/api/specific/path")
+    
     ## Anon.7.0 End 
 
-## Anon.U Anonymous User
+# Anon.U Anonymous User
 class AnonymousUser(HttpLocust):
-    host = os.getenv('TARGET_URL', "http://dev-ps-loadtest-dummy.pantheonsite.io")
+    host = os.getenv('TARGET_URL', "https://dev-ps-loadtest-dummy.pantheonsite.io")
     task_set = AnonymousTaskSet
     wait_time = between(1, 3)
 
@@ -242,7 +237,7 @@ class AuthenticatedTaskSet(TaskSet):
         self.login() # on_start is called when a Locust start before, any task is scheduled
 
     def login(self):
-        #login function using http session handler. Modify for the actual login path
+        # login function using http session handler. Modify for the actual login path
         request_path = "/user/login"
         self.client.post(request_path, {
             "name": "umami",
@@ -259,7 +254,7 @@ class AuthenticatedTaskSet(TaskSet):
     @task(1)
     def navigate_site_steps(self):
         self.client.get("/")
-        self.client.get("/en/user/4/edit")
+        self.client.get("/user/1/edit")
         # Add more paths or pages here    
     ## Auth.1.0 End
 
@@ -290,14 +285,13 @@ class AuthenticatedTaskSet(TaskSet):
             "form_build_id": get_form_build_id(self, request_path),
             "op": "Save"
         })
-        
     ## 2.0 End
 
     ## 3.0 Adding Comments to Articles
     # Scenario: Configured to only post as Authenticated
     # Make sure that BigPipe is turned-off in Drupal 8 
     # The response content is not complete and unable to be parsed by lxml
-    @task(100)
+    @task(1)
     def create_node_article_comment(self):
         fake = Faker() # Create new Faker instance
         
@@ -306,7 +300,9 @@ class AuthenticatedTaskSet(TaskSet):
         random_comment = get_random_article(self, "/articles", "read-more__link", "comment-form")
         comment_post_path = random_comment["random_article_path"]
         random_article_path = random_comment["comment_post_path"]
-
+        print("Random Article: " + random_article_path)
+        print("Comment Post Path: " + comment_post_path)
+        
         # Post the Comment to the selected 
         self.client.post(comment_post_path, {
             "subject[0][value]": fake.sentence(),
@@ -319,7 +315,33 @@ class AuthenticatedTaskSet(TaskSet):
         })
     ## 3.0 End
 
-    ## 4.0 Create an article via JSON:API
+    ## TODO - 4.0 Create an article via JSON:API
+    # @task(100)
+    # def jsonapi_crete_node_article(self):
+    #     # Create new Faker instance and text
+    #     fake = Faker() 
+    #     article_title = fake.format('sentence')
+    #     article_body = fake.format('text')
+    #     article_tags = fake.format('word')
+    #     print(article_title)
+    #     print(article_body)
+    #     print(article_tags)
+        
+    #     payload = { 
+    #                 "data": {
+    #                     "type": "node--article",
+    #                     "attributes": {
+    #                         "title": article_title,
+    #                         "body": {
+    #                             "value": article_body,
+    #                             "format": "plain_text"
+    #                             }
+    #                         }
+    #                     }
+    #                 }
+
+    #     endpoint = "/jsonapi/node/article"
+    #     self.client.post(endpoint, data=json.dumps(payload), headers={'Accept: application/vnd.api+json', 'Content-Type': 'application/vnd.api+json'}, auth=('umami', 'umami'))  
     ## 4.0 End
 
     ## 5.0 Commerce Shopping Cart Steps
@@ -327,25 +349,25 @@ class AuthenticatedTaskSet(TaskSet):
 
 # Auth.U Authenticated User
 class AuthenticatedUser(HttpLocust):
-    host = os.getenv('TARGET_URL', "http://dev-ps-loadtest-dummy.pantheonsite.io")
+    host = os.getenv('TARGET_URL', "https://dev-ps-loadtest-dummy.pantheonsite.io")
     task_set = AuthenticatedTaskSet
-    wait_time = between(6, 20)
+    wait_time = between(5, 20)
 
 ## Adv.T Custom Scripts, CasperJS, Selenium, PyTest etc
-class AdvancedTaskSet(TaskSet):
+# class AdvancedTaskSet(TaskSet):
 
-    ## Adv.6.0 Recorded Selenium Steps
-    @task(1)
-    def pytest(self):
-         start_time = time.time()
-         os.system("pytest locustfile.py")
-         total_time = int((time.time() - start_time) * 1000)
-         events.request_success.fire(
-             request_type="pytest",
-             name="login-search",
-             response_time=total_time,
-             response_length=0
-         )
+#     ## Adv.6.0 Recorded Selenium Steps
+#     @task(1)
+#     def pytest(self):
+#          start_time = time.time()
+#          os.system("pytest locustfile.py")
+#          total_time = int((time.time() - start_time) * 1000)
+#          events.request_success.fire(
+#              request_type="pytest",
+#              name="login-search",
+#              response_time=total_time,
+#              response_length=0
+#          )
 
     #@task(1)
     #def selenium(self):
@@ -361,6 +383,7 @@ class AdvancedTaskSet(TaskSet):
 
 ## Adv.U Advanced User Setting
 # class AdvancedUser(HttpLocust):
-#     host = os.getenv('TARGET_URL', "http://dev-ps-loadtest-dummy.pantheonsite.io")
+#     host = os.getenv('TARGET_URL', "https://dev-ps-loadtest-dummy.pantheonsite.io")
 #     task_set = AdvancedTaskSet
-#     wait_time = between(1, 3)
+#     wait_time = between(6, 10)
+
